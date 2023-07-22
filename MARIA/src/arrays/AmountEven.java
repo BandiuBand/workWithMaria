32eb1ccf076ed9db7a14dc55154fbb8d9a31090b
Bandiu <Bandiubandi@gmail.com>
2023-07-22 21:37:27 +0300

    create start method of AmountEven class

--- MARIA/src/arrays/AmountEven.java
+++ MARIA/src/arrays/AmountEven.java
@@ -1,4 +1,12 @@
 package arrays;
 
 public class AmountEven {
+    public static int start (int[] array) {
+        int result = 0;
+        for (int i = 0; i < array.length; i++) {
+            if (array[i] % 2 == 0)
+                result++;
+        }
+        return result;
+    }
 }
