c5aec90639051d15f60f84f32c6e0ea2fbf1062e
Bandiu <Bandiubandi@gmail.com>
2023-07-22 21:25:07 +0300

    create start method of MAX

--- MARIA/src/arrays/MAX.java
+++ MARIA/src/arrays/MAX.java
@@ -1,4 +1,12 @@
 package arrays;
 
 public class MAX {
+    public static int start (int[] array)
+    {   int result = array[0];
+        for (int i = 1; i < array.length; i++) {
+            if (array[i] > result)
+                result = array[i];
+        }
+        return result;
+    }
 }
