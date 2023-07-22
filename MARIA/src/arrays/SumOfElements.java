2a251c5beb2671b928664723a69943329f72ec34
Bandiu <Bandiubandi@gmail.com>
2023-07-22 21:13:04 +0300

    create SumOfElements class and create method for int

---
+++ MARIA/src/arrays/SumOfElements.java
@@ -0,0 +1,22 @@
+package arrays;
+
+public class SumOfElements {
+    public static int start (int[] array)
+    {
+        int result = 0;
+        for (int i = 0; i < array.length; i++) {
+            result = +array[i];
+        }
+        return result;
+    }
+    /*public static <T> T start(T[] array)
+    {
+        Object t =  new Object();
+        for (int i = 0; i < array.length; i++) {
+            t = (Object)(((T)t) + array[i] );
+        }
+        return (T)t;
+    }
+
+     */
+}
