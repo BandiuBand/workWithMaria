df4dcf8fca40a1a9cad4f84198e27110c4f83f2f
Bandiu <Bandiubandi@gmail.com>
2023-07-22 21:23:55 +0300

    create start method of MIN class

--- MARIA/src/arrays/MIN.java
+++ MARIA/src/arrays/MIN.java
@@ -1,4 +1,12 @@
 package arrays;
 
 public class MIN {
+    public static int start (int[] array)
+    {   int result = array[0];
+        for (int i = 1; i < array.length; i++) {
+            if (array[i] < result)
+                result = array[i];
+        }
+        return result;
+    }
 }
