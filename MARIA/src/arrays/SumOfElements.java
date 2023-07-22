96f0d0b128e7f7411cb203dd9dddb0387985dfff
Bandiu <Bandiubandi@gmail.com>
2023-07-22 21:14:34 +0300

    app result in the 8 line

--- MARIA/src/arrays/SumOfElements.java
+++ MARIA/src/arrays/SumOfElements.java
@@ -5,7 +5,7 @@ public class SumOfElements {
     {
         int result = 0;
         for (int i = 0; i < array.length; i++) {
-            result = +array[i];
+            result = result+array[i];
         }
         return result;
     }
