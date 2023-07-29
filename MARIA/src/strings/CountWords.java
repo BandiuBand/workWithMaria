2fa15cc513f927dbc0419cc4fc613a785536be1f
Bandiu <Bandiubandi@gmail.com>
2023-07-29 19:13:36 +0300

    modify for work with Cirilic too

--- MARIA/src/strings/CountWords.java
+++ MARIA/src/strings/CountWords.java
@@ -6,7 +6,7 @@ import java.util.regex.Pattern;
 public class CountWords {
     public static int start (String string)
     {
-        Pattern pattern = Pattern.compile("\\w+\\s*\\W*");
+        Pattern pattern = Pattern.compile("\\p{L}+");
         Matcher matcher = pattern.matcher(string);
         int i = 0;
         while (matcher.find())
